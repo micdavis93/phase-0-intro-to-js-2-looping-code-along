da02c5dadfc4901f2a86fc9e70cc6e07ccb14183
Mike Davis <micdavis93@gmail.com>
2022-11-03 11:57:11 -0600

    commit tree f845e92b19eae4e4c923f5e238a2540ac4772b48

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Code your solutions in this file
+function writeCards(names, occasion) {
+    const messages = [];
+        for (let name of names) {
+            messages.push(`Thank you, ${name}, for the wonderful ${occasion} gift!`);
+        }
+    console.log(messages)
+}
